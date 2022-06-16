121be1fa8a3cb21f102c364e2945c19a58f1ce28
ScottDeev <99127718+ScottDeev@users.noreply.github.com>
2022-06-16 23:33:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 
 form.addEventListener('submit', e => {
-	e.preventDefault();
 	
 	checkInputs();
 });
